ae0fe2037a2b71ba38163ecbba22e05509d3f756
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2022-05-24 15:14:35 +0200

    decreased the size of pieces in chess and switched numbers and characters

--- src/Games/Chess/Chess.scala
+++ src/Games/Chess/Chess.scala
@@ -10,39 +10,38 @@ import javax.swing.{JPanel}
 object Chess extends {
   case class Piece(name: String, side: String, i: Int, j: Int) extends Drawable {
     override var img: Image = ImageIO.read(new File(s"src/Games/Chess/assets/$side/$name.png"))
-                                      .getScaledInstance(60, 60, Image.SCALE_SMOOTH)
+                                      .getScaledInstance(30, 30, Image.SCALE_SMOOTH)
     override var x: Int = i
     override var y: Int = j
   }
 
   class ChessState extends State {
     this.drawables = Array.ofDim[Drawable](8,8)
-    this.drawables(0)(0) = Piece("rook", "white", 0, 0)
-    this.drawables(0)(1) = Piece("knight", "white", 0, 1)
-    this.drawables(0)(2) = Piece("bishop", "white", 0, 2)
-    this.drawables(0)(3) = Piece("queen", "white", 0, 3)
-    this.drawables(0)(4) = Piece("king", "white", 0, 4)
-    this.drawables(0)(5) = Piece("bishop", "white", 0, 5)
-    this.drawables(0)(6) = Piece("knight", "white", 0, 6)
-    this.drawables(0)(7) = Piece("rook", "white", 0, 7)
+    this.drawables(0)(0) = Piece("rook", "black", 0, 0)
+    this.drawables(0)(1) = Piece("knight", "black", 0, 1)
+    this.drawables(0)(2) = Piece("bishop", "black", 0, 2)
+    this.drawables(0)(3) = Piece("queen", "black", 0, 3)
+    this.drawables(0)(4) = Piece("king", "black", 0, 4)
+    this.drawables(0)(5) = Piece("bishop", "black", 0, 5)
+    this.drawables(0)(6) = Piece("knight", "black", 0, 6)
+    this.drawables(0)(7) = Piece("rook", "black", 0, 7)
     for(i <- 0 until(8) ) {
-      this.drawables(1)(i) = Piece("pawn", "white", 1, i)
-      this.drawables(6)(i) = Piece("pawn", "black", 6, i)
+      this.drawables(1)(i) = Piece("pawn", "black", 1, i)
+      this.drawables(6)(i) = Piece("pawn", "white", 6, i)
     }
-    this.drawables(7)(0) = Piece("rook", "black", 7, 0)
-    this.drawables(7)(1) = Piece("knight", "black", 7, 1)
-    this.drawables(7)(2) = Piece("bishop", "black", 7, 2)
-    this.drawables(7)(3) = Piece("queen", "black", 7, 3)
-    this.drawables(7)(4) = Piece("king", "black", 7, 4)
-    this.drawables(7)(5) = Piece("bishop", "black", 7, 5)
-    this.drawables(7)(6) = Piece("knight", "black", 7, 6)
-    this.drawables(7)(7) = Piece("rook", "black", 7, 7)
+    this.drawables(7)(0) = Piece("rook", "white", 7, 0)
+    this.drawables(7)(1) = Piece("knight", "white", 7, 1)
+    this.drawables(7)(2) = Piece("bishop", "white", 7, 2)
+    this.drawables(7)(3) = Piece("queen", "white", 7, 3)
+    this.drawables(7)(4) = Piece("king", "white", 7, 4)
+    this.drawables(7)(5) = Piece("bishop", "white", 7, 5)
+    this.drawables(7)(6) = Piece("knight", "white", 7, 6)
+    this.drawables(7)(7) = Piece("rook", "white", 7, 7)
   }
 
   def init(): State = {
     new ChessState()
   }
-
 
   def drawer(state: State): JPanel = {
     val alpha = Array("a", "b", "c", "d", "e", "f", "g", "h")
@@ -56,19 +55,17 @@ object Chess extends {
             else g.setColor(new Color(119, 148, 85))
             g.fillRect(10 + x * 60, 10 + y * 60, 60, 60)
             alternateColor = !alternateColor
-            g.setColor(Color.black)
-            if(y == 0 || y == 7) {
-              g.drawString((x+1).toString, 35 + x * 60, 10)
-              g.drawString((x+1).toString, 35 + x * 60, 500)
-            }
             val p = state.drawables(y)(x)
             if(p != null) {
-              g.drawImage(p.img, 10 + p.y * 60, 10 + p.x * 60, null)
+              g.drawImage(p.img, 25 + p.y * 60, 25 + p.x * 60, null)
             }
           }
           alternateColor = !alternateColor
-          g.drawString(alpha(y), 0, 40 + y * 60)
-          g.drawString(alpha(y), 492, 40 + y * 60)
+          g.setColor(Color.black)
+          g.drawString((8-y).toString, 0, 40 + y * 60)
+          g.drawString((8-y).toString, 492, 40 + y * 60)
+          g.drawString(alpha(y), 35 + y * 60, 10)
+          g.drawString(alpha(y), 35 + y * 60, 500)
         }
       }
     }
